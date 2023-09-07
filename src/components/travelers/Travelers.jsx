00fe8d5f2c90c46f74acee6b3ab64df90ee8a2c3
mouhamed <mouhamed.mbaye1@outlook.com>
2023-09-07 02:43:28 +0200

    finishing travelers.jsx

--- src/components/travelers/Travelers.jsx
+++ src/components/travelers/Travelers.jsx
@@ -1,4 +1,5 @@
 import './travelers.css';
+import { travelers } from '../../data/tarvelers';
 
 const Travelers = () => {
     return (
@@ -6,18 +7,29 @@ const Travelers = () => {
             <div className='sectionContainer'>
                 <h2>Top travelers of this month !</h2>
                 <div className='travelersContainer grid'>
-                    <div className='singleTraveler'>
-                        <img className='destinationImg' src="" alt="" />
-                        <div className='travelerDetails'>
-                            <div className='travelerPic'>
-                                <img src="" alt="" />
-                            </div>
-                        </div>
-                    </div>
+                    {
+                        travelers.map(({ id, destinationImg, travelerImg, travelerName, socialLink }) => {
+                            return (
+                                <div key={id} className='singleTraveler'>
+                                    <img className='destinationImg' src={destinationImg} alt="" />
+                                    <div className='travelerDetails'>
+                                        <div className='travelerPic'>
+                                            <img className='travelerImg' src={travelerImg} alt="" />
+                                        </div>
+                                        <div className='travelerName'>
+                                            <span>{travelerName}</span>
+                                            <p>{socialLink}</p>
+                                        </div>
+                                    </div>
+                                </div>)
+
+                        })
+                    }
+
                 </div>
             </div>
-
         </div>
+
     )
 }
 
---
+++ src/data/tarvelers.js
@@ -0,0 +1,21 @@
+import destination1 from '../assets/destination.jpg';
+import traveler1 from '../assets/traveler.jpeg'
+import destination2 from '../assets/destination2.jpg';
+import traveler2 from '../assets/traveler2.jpg'
+
+export const travelers = [
+    {
+        id: 1,
+        destinationImg: destination1,
+        travelerImg: traveler1,
+        travelerName: 'Modough',
+        socialLink: '@modough23'
+    },
+    {
+        id: 2,
+        destinationImg: destination2,
+        travelerImg: traveler2,
+        travelerName: 'badlick',
+        socialLink: '@badlick81'
+    }
+]
